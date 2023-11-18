e0140683e3818ae1c27f92542659486e81956381
Leonardo Barcia <barcialeonardoexequiel@gmail.com>
2023-11-18 20:41:01 -0300

    commint de los dominios
    dominios asociados al proyecto

---
+++ InformaticaCliente/Aplicacion/Dominios/Formas_Envio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiendainformatica.Entidades
+{
+    public class Formas_Envio
+    {
+        public int Id_Forma_Envio { get; set; }
+
+        public string Descripcion { get; set; }
+        public Formas_Envio(int id_Forma_Envio, string descripcion)
+        {
+            Id_Forma_Envio = id_Forma_Envio;
+            Descripcion = descripcion;
+        }
+
+    }
+}
---
+++ InformaticaCliente/Aplicacion/Dominios/Vendedores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiendainformatica.Entidades
+{
+    public class Vendedores
+    {
+        public int Id_vendedor { get; set; }
+        public string NombreVendedor { get; set; }
+        public string ApellidoVendedor { get; set; }
+        public int num_doc { get; set; }
+        public int ID_Tipo_Doc { get; set; }
+        public int Id_Contacto { get; set; }
+        public Vendedores(int id_vendedor, string nombreVendedor, string apellidoVendedor, int num_doc, int iD_Tipo_Doc, int id_Contacto)
+        {
+            Id_vendedor = id_vendedor;
+            NombreVendedor = nombreVendedor;
+            ApellidoVendedor = apellidoVendedor;
+            this.num_doc = num_doc;
+            ID_Tipo_Doc = iD_Tipo_Doc;
+            Id_Contacto = id_Contacto;
+        }
+    }
+}
